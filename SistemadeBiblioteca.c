cbf831cc5c8d7074a00f047824c14ef270b49e22
nandnlopes9 <nandolopes9@gmail.com>
2023-11-18 17:06:06 -0300

    deu

--- SistemadeBiblioteca.c
+++ SistemadeBiblioteca.c
@@ -4,13 +4,25 @@ typedef struct{
 }Biblioteca;
 
 typedef struct Autor{
-    char nomeA[100];
-    char instituicao;   
+    char nomeA[100],instituicao[100];   
 }Autor;
 
+typedef struct Livro{
+    int idlivro, ano, edicao;
+    char tituloLivro[100], editora[100];
+    Autor nome;
+}Livro;
 typedef struct Usuario{
-    int idUser;
+    int idUser, telefone;
+    char nomeUser[100],endereco[200];
+
 }Usuario;
+
+typedef struct Reserva{
+    int dataInicio, dataFim;
+    Usuario idUser;
+    Livro idLivro;
+}Reserva;
 
 int main(){
 
