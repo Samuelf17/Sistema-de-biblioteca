0377bf26122635f34546500d7c92aa334c93fb12
nandnlopes9 <nandolopes9@gmail.com>
2023-11-18 16:52:34 -0300

    foda

--- SistemadeBiblioteca.c
+++ SistemadeBiblioteca.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+typedef struct{
+    
+}Biblioteca;
 
+typedef struct Autor{
+    char nomeA[100];
+    char instituicao;   
+}Autor;
+
+typedef struct Usuario{
+    int idUser;
+}Usuario;
 
 int main(){
 
